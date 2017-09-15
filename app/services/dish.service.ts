2f2ffc674d2f1191accbb6a582539f2d7e81efcc
Claudio Giovane <claudiogiovane@gmail.com>
2017-09-15 11:40:26 -0300

    NativeScript Angular

---
+++ app/services/dish.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core'
+
+import { Observable } from 'rxjs/Observable'
+import { Http, Response } from '@angular/http'
+
+import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/delay'
+import 'rxjs/add/operator/catch'
+
+import { Dish } from '../shared/dish'
+import { baseURL } from '../shared/baseurl'
+
+import { ProcessHTTPMsgService } from './process-httpmsg.service'
+
+Injectable()
+
+export class DishService {
+
+  constructor(public http: Http, 
+              private processHTTPMsgService: ProcessHTTPMsgService) { }
+
+  getDishes(): Observable<Dish[]> {
+     return this.http.get(baseURL + 'dishes')
+     .map(res => {return this.processHTTPMsgService.extractData(res)})
+     .catch(error => {return this.processHTTPMsgService.handleError(error)})
+  } 
+
+   getDish(id: number): Observable<Dish> {
+     return this.http.get(baseURL + 'dishes/' + id)
+     .map(res => {return this.processHTTPMsgService.extractData(res)})
+     .catch(error => {return this.processHTTPMsgService.handleError(error)})
+  }
+  
+  getFeaturedDish(): Observable<Dish> {
+     return this.http.get(baseURL + 'dishes?featured=true')
+     .map(res => {return this.processHTTPMsgService.extractData(res)[0]})
+     .catch(error => {return this.processHTTPMsgService.handleError(error)})
+  }
+
+}
---
+++ app/shared/baseurl.ts
@@ -0,0 +1,1 @@
+export const baseURL = "http://10.65.10.148:3001/"
